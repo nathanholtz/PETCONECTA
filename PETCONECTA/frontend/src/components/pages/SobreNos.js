5582e3b171a3e181151a5c4757a1d8dbef4b8086
HOLTZ <nathan.holtz0805@gmail.com>
2025-06-09 13:40:34 -0300

    modified:   PETCONECTA/frontend/src/components/pages/SobreNos.js

--- PETCONECTA/frontend/src/components/pages/SobreNos.js
+++ PETCONECTA/frontend/src/components/pages/SobreNos.js
@@ -5,7 +5,7 @@ import '../estilos/SobreNos.css';
 function SobreNos() {
     return(
         <><h1>Página Sobre Nós</h1>
-        <p>Somos um grupo de estudantes da Fatec Baixada Santista, com o objetivo de foidase</p></>
+        <p>Somos um grupo de estudantes da Fatec Baixada Santista</p></>
     );
 }
 
